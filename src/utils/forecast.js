f2391fb344cf02277c5903ffff8d10b6b90e889f
Justin Steele <Justin@Justins-MacBook-Air-2.local>
2022-06-14 20:55:43 -0400

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=ac1d57ebd9a11f05b3f91fc6a02a3d70&query=' + longitude + ',' + latitude + '&units=f'
+
+    request({ url, json: true}, (error, { body } = {} ) => {
+        if(error) {
+            callback('No connection to service.', undefined)
+        } else if (body.error) {
+            callback('Unable to find location, please specify location.', undefined)
+        } else {
+            temp = body.current.temperature,
+            feel = body.current.feelslike,
+            descript = body.current.weather_descriptions[0]
+            callback(undefined, descript + '. It is currently ' + temp + ' degrees out. It feels like ' + feel + ' degrees out.')
+        }
+    })
+}
+module.exports = forecast
